e62686a9d50183a2a4f9e5c207b6742e89b4149c
Dennis Shtatnov <densht@gmail.com>
2016-10-10 20:58:01 -0400

    Only send back essential data

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -2063,7 +2063,8 @@ Mavlink::task_main(int argc, char *argv[])
 		break;
 
 	case MAVLINK_MODE_MAGIC:
-		//stream nothing
+		//stream nothing not vital to monitoring system health
+		configure_stream("SYS_STATUS", 0.5f);
 		break;
 
 	case MAVLINK_MODE_CONFIG:
